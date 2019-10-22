127ef8acc8e26c9733c35185980a901666920063
qintian <qintian@000u.cc>
2019-10-22 21:02:38 +0800

    开奖页
    Signed-off-by: qintian <qintian@000u.cc>

--- app/src/main/java/com/fenghuang/caipiaobao/ui/lottery/LotteryOpenCodeAdapter.kt
+++ app/src/main/java/com/fenghuang/caipiaobao/ui/lottery/LotteryOpenCodeAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.ViewGroup
 import com.fenghuang.baselib.base.recycler.BaseRecyclerAdapter
 import com.fenghuang.baselib.base.recycler.BaseViewHolder
+import com.fenghuang.baselib.widget.round.RoundTextView
 import com.fenghuang.caipiaobao.R
 
 /**
@@ -21,6 +22,18 @@ class LotteryOpenCodeAdapter(context: Context) : BaseRecyclerAdapter<String>(con
 
     inner class LotteryTypeHolder(parent: ViewGroup) : BaseViewHolder<String>(getContext(), parent, R.layout.holder_lottery_opencode) {
         override fun onBindData(data: String) {
+            when (data) {
+                "1" -> findView<RoundTextView>(R.id.tvOpenCode).delegate.backgroundColor = getColor(R.color.color_F8E96B)
+                "2" -> findView<RoundTextView>(R.id.tvOpenCode).delegate.backgroundColor = getColor(R.color.color_73CCF9)
+                "3" -> findView<RoundTextView>(R.id.tvOpenCode).delegate.backgroundColor = getColor(R.color.color_888888)
+                "4" -> findView<RoundTextView>(R.id.tvOpenCode).delegate.backgroundColor = getColor(R.color.color_FAA056)
+                "5" -> findView<RoundTextView>(R.id.tvOpenCode).delegate.backgroundColor = getColor(R.color.color_5EF0BA)
+                "6" -> findView<RoundTextView>(R.id.tvOpenCode).delegate.backgroundColor = getColor(R.color.color_6653DD)
+                "7" -> findView<RoundTextView>(R.id.tvOpenCode).delegate.backgroundColor = getColor(R.color.color_BBBBBB)
+                "8" -> findView<RoundTextView>(R.id.tvOpenCode).delegate.backgroundColor = getColor(R.color.color_F8767A)
+                "9" -> findView<RoundTextView>(R.id.tvOpenCode).delegate.backgroundColor = getColor(R.color.color_E03940)
+                "10" -> findView<RoundTextView>(R.id.tvOpenCode).delegate.backgroundColor = getColor(R.color.color_84EC7F)
+            }
             setText(R.id.tvOpenCode, data)
         }
 
